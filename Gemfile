09f1765404d23bd3268c5b8118584b84b6dfa8b6
Raj <raj_abc38@yahoo.com>
2017-05-28 00:24:09 +0530

    Installed simplecov for test coverage info

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.3.4'
 gem 'rspec'
-gem 'rubocop'
+gem 'rubocop', require: false
+gem 'simplecov', require: false, group: :test
+gem 'byebug', group: [:development, :test]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@
 
 Dir[File.join('./lib/**/*.rb')].each { |f| require f }
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
