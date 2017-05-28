0ca8e287b383f86e14bc61f8d275fba5a007f507
Raj <raj_abc38@yahoo.com>
2017-05-28 19:21:16 +0530

    RDoc Documention

--- spec/lib/bitmap_editor_spec.rb
+++ spec/lib/bitmap_editor_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe BitmapEditor do
-  # subject(:bitmap_editor) { described_class.new }
-  #
-  # let(:file) { './spec/factories/basic.txt' }
+  subject(:bitmap_editor) { described_class.new }
 
-  # describe '#run' do
-  #   it { expect(subject.run(file)).to output(/0000/).to_stdout }
-  # end
+  let(:file) { './spec/factories/basic.txt' }
+
+  describe '#run' do
+    it 'should make a call to `plot_bitmap` with file path' do
+      expect(bitmap_editor).to receive(:plot_bitmap).with(file)
+      bitmap_editor.run(file)
+    end
+  end
 end
